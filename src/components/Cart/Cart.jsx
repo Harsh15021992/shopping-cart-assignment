a7b4bef452d4c56108224cbd262dca8ca7793835
Harsh Pandey <harsh15pandey@gmail.com>
2021-07-12 19:16:26 +0530

    added test cases for cart and cart Item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,55 +1,12 @@
 import "./Cart.scss";
 import { useContext } from "react";
-import { Container, Modal, Row, Col, Image, Button } from "react-bootstrap";
+import { Container, Modal, Button } from "react-bootstrap";
 import shoppingContext from "../../context/shopping.context";
+import CartItem from "./CartItem";
 
 const Cart = ({ show, handleClose }) => {
   const shoppingData = useContext(shoppingContext);
-  const { cart, updateCart } = shoppingData;
-
-  const CartItem = ({ item, index }) => {
-    const updateItem = (type) => {
-      let newCart = [...cart];
-      if (type === "decrease") {
-        if (newCart[index].count !== 1) {
-          newCart[index].count -= 1;
-        } else {
-          newCart.splice(index, 1);
-        }
-      } else if (type === "increase") {
-        newCart[index].count += 1;
-      }
-      updateCart(newCart);
-    };
-    return (
-      <Row className="cart-item">
-        <Col xs={3}>
-          <Image src={`${process.env.PUBLIC_URL}${item.imageURL}`} thumbnail />
-        </Col>
-        <Col xs={9} className="pl-0">
-          <h5>{item.name}</h5>
-          <Button
-            variant="primary"
-            className="theme-button decrease-count"
-            onClick={() => updateItem("decrease")}
-          >
-            -
-          </Button>
-          <span>{item.count}</span>
-          <Button
-            variant="primary"
-            className="theme-button increase-count"
-            onClick={() => updateItem("increase")}
-          >
-            +
-          </Button>
-          <span>x</span>
-          <span>Rs.{item.price}</span>
-          <span className="total">Rs.{item.count * item.price}</span>
-        </Col>
-      </Row>
-    );
-  };
+  const { cart } = shoppingData;
 
   return (
     <Modal show={show} onHide={handleClose}>
--- src/components/Cart/CartItem.test.jsx
+++ src/components/Cart/CartItem.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Cart from "./Cart";
+import CartItem from "./CartItem";
 import Adapter from "enzyme-adapter-react-16";
 import { shallow, configure } from "enzyme";
 
@@ -71,19 +71,64 @@ const sampleContextData = {
   updateProducts: () => {},
   updateCart: () => {},
 };
-const sampleEmptyContextData = {
-  offers: [],
-  categories: [],
-  products: [],
-  cart: [],
+const sampleContextDatawithSingleCartItem = {
+  offers: [
+    {
+      bannerImageUrl: "/static/images/offers/offer1.jpg",
+      bannerImageAlt: "Independence Day Deal - 25% off on shampoo",
+      isActive: true,
+      order: 1,
+      id: "5b6c38156cb7d770b7010ccc",
+    },
+  ],
+  categories: [
+    {
+      name: "Fruits & Vegetables",
+      key: "fruit-and-veg",
+      description: "A variety of fresh fruits and vegetables.",
+      enabled: true,
+      order: 1,
+      imageUrl: "/static/images/category/fruits.png",
+      id: "5b6899953d1a866534f516e2",
+    },
+  ],
+  products: [
+    {
+      name: "Capsicum - Green, 1 kg",
+      imageURL: "/static/images/products/fruit-n-veg/capsicum-green.jpg",
+      description:
+        "Leaving a moderately pungent taste on the tongue, Green capsicums, also known as green peppers are bell shaped, medium-sized fruit pods.",
+      price: 137,
+      stock: 50,
+      category: "5b6899953d1a866534f516e2",
+      sku: "fnw-capsicum-1",
+      id: "5b6c6bdc01a7c38429530886",
+    },
+  ],
+  cart: [
+    {
+      name: "Capsicum - Green, 1 kg",
+      imageURL: "/static/images/products/fruit-n-veg/capsicum-green.jpg",
+      description:
+        "Leaving a moderately pungent taste on the tongue, Green capsicums, also known as green peppers are bell shaped, medium-sized fruit pods.",
+      price: 137,
+      stock: 50,
+      category: "5b6899953d1a866534f516e2",
+      sku: "fnw-capsicum-1",
+      id: "5b6c6bdc01a7c38429530886",
+      count: 1,
+    },
+  ],
   updateOffers: () => {},
   updateCategories: () => {},
   updateProducts: () => {},
   updateCart: () => {},
 };
+
 const baseProps = {
-  show: true,
-  handleClose: jest.fn(),
+  item: sampleContextData.cart[0],
+  index: 0,
+  key: 0,
 };
 
 beforeEach(() => {
@@ -94,16 +139,30 @@ beforeEach(() => {
 afterEach(() => {
   React.useContext = realUseContext;
 });
+
 describe("Cart Component test Suit", () => {
-  it("Cart should match with snapshot", () => {
+  it("CartItem should match with snapshot", () => {
     useContextMock.mockReturnValue(sampleContextData);
-    const tree = shallow(<Cart {...baseProps} />);
+    const tree = shallow(<CartItem {...baseProps} />);
     expect(tree).toBeTruthy();
     expect(tree).toMatchSnapshot();
   });
-  it("Empty cart should render any item", () => {
-    useContextMock.mockReturnValue(sampleEmptyContextData);
-    const tree = shallow(<Cart {...baseProps} />);
-    expect(tree).toBeTruthy();
+  it("decrease item click test", () => {
+    useContextMock.mockReturnValue(sampleContextData);
+    const tree = shallow(<CartItem {...baseProps} />);
+    const decreaseCountButton = tree.find(".decrease-count");
+    decreaseCountButton.simulate("click", "decrease");
+  });
+  it("decrease item click should remove item from cart permanently if count is 1", () => {
+    useContextMock.mockReturnValue(sampleContextDatawithSingleCartItem);
+    const tree = shallow(<CartItem {...baseProps} />);
+    const decreaseCountButton = tree.find(".decrease-count");
+    decreaseCountButton.simulate("click", "decrease");
+  });
+  it("increase item click test", () => {
+    useContextMock.mockReturnValue(sampleContextData);
+    const tree = shallow(<CartItem {...baseProps} />);
+    const increaseCountButton = tree.find(".increase-count");
+    increaseCountButton.simulate("click", "increase");
   });
 });
